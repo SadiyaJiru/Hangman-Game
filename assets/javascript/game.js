046f9931e27fb4eef5b908402b93c59fee33dad3
sadiyaj <sadiyajiru@gmail.com>
2018-07-23 22:14:32 -0500

    better design

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,114 @@
-var $ = function (id){
-    return document.getElementById(id);
-}
+	//Globabl Variables
+	//array that includes the hangman words
+	var movies = ['deadpool','grease','batman','avatar','shrek','titanic','goodfellas']
+	var	wins = 0; //// counter for # of wins
+	var	loss = 0; // sets the stat
+	var	guessesLeft = 15; // 26 guesses to start with 
+	var	wrongLetter =[];
+	var rightLetter =[];
+	var	underScores =[]; // creates the space for each word 
+	var	userGuess =[];
+	var	randMovie; //picks random movie from array
+	var winningLetters = 0; //# of correct letters
 
- // DECALRE global scope VARIABLRS 
-var movies = ["Forest Gump", "Step Brothers", "Titanic"] // answers
-var guess = Math.floor(Math.random()*4);// 
-var answer = movies[guess];
-var myLength = answer.length; // counts the letters in the word 
-var display=[myLength]; // the _ _ _ for each letter
-var win = myLength; // they have to guess that many letters to guess it
-var letters = answer.split(''); // creates an array called letters from answer, and 
-//breaks the letters apart, each char is at an index
-var chancesLeft = 26; //
-var ouput = ""; // how ---- is displayed
-var userLetter= ""; // obtained from the user
-
-//  var guess = prompt("Guess a movie")
-
- var setup = function(){
-     //for loop for working with our array
-     for(var i =0; i < answer.length; i++);{
-         display[i] = "_ ";
-         ouput = ouput + display[i];
-     }
-document.getElementById("movies").innerHTML.ouput;
-output = "";
-    }
-    window.onload = function()
-    {
+	//Function
+	//===========================
 
 
-    setup();
-}
-///// WHEN THE USER HITS GUESS////
-var sumbit = function(){
-    ouput = ""; // reset output to nothing
-    userLetter = $("letter").value; // user letter is the value of whats in the text box by the user
-    $("letter").value = ""; //
+	//picks a random movie using math.random function
+	function startGame()
+	{
+		randMovie = movies[Math.floor(Math.random()* movies.length)] 
+		
+		// console.log(randMovie);
+		underScores = [];
 
-    for (var c=0; c< answer.length; c++){ // step through the loop for each letter in the answer
-        allert(letters[c]); //checks if the letter input is in the answer
-       if (userLetter.toUpperCase() == letters[c]) 
-{
-    display[c]=userLetter.toUpperCase(); ///
-    win --;
-}
-output=output + display[c] + ""; // reads diplay
-    }
-document.getElementById("movies").innerHTML = output; // redisplay the output
-ouput = "";
-chancesLeft --;
-if (win < 1)
-{
-    document.getElementById("guess").innerHTML = "YOU WIN";
-}
-}
+		for(var i = 0; i < randMovie.length; i ++){
+				underScores.push('_'); // new item to the array
+		}
+		//printing underscores to screen with blanks spaces in between		
+		document.getElementById('word-blanks').innerHTML = underScores.join(" ");
+		document.getElementById('guessRemain').innerHTML = guessesLeft;
+
+		//empty array for wrong letters, 
+		wrongLetter =[];
+		guessesLeft =15;
+				
+	}
+	//reset all stats and storage/arrays
+	function reset(){
+		underScores = [];	
+		wrongLetter =[];
+		guessesLeft = 15;
+		winningLetters = 0;
+		lossCounter = 0;
+		
+	}
+	
+	//if the users letter guess is wrong go to next funtion
+	function checkLetter(){
+		if(userGuess === wrongLetter ){
+
+		}
+	}
+
+	function winLose()
+	{
+		if(winningLetters == randMovie.length){
+			alert('You win WIN the movie is ' + randMovie + ' Which is a great movie');
+			wins ++;
+			document.getElementById('wins').innerHTML = wins;
+			reset();
+
+		//If there are no more gueses left the user loses
+		}else if(guessesLeft <= 0){
+			alert('You Lose The Movie is ' + randMovie);
+			loss ++; 
+			document.getElementById('loses').innerHTML = loss;
+			reset();
+		}
+	}
+
+	//userInput
+	document.onkeyup = function(event)
+	{
+		userGuess = event.key;
+
+	//	console.log(userGuess);
+
+		//checks if user movie matches guesses
+		if(randMovie.indexOf(userGuess) > -1)
+		{
+			for(var i = 0; i < randMovie.length; i++)
+			{
+				if(randMovie[i] === userGuess)
+				{
+					underScores[i] = userGuess;
+					document.getElementById('word-blanks').innerHTML = underScores.join(' ');
+					console.log(underScores);
+					winningLetters++;
+					console.log(winningLetters);
+					winLose();
+				}
+			}
+		}		
+		else
+		{
+			wrongLetter.push(userGuess);
+			guessesLeft--;
+			//send guessesLeft to HTML
+			document.getElementById('guessRemain').innerHTML = guessesLeft;
+			document.getElementById('wrongGuess').innerHTML = wrongLetter;
+			console.log("Guess left" + guessesLeft);	
+			winLose();
+				
+		}
+	}
+
+	
+
+
+	//Main
+	//============================
+
+	
